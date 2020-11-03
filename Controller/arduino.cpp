de7d47fc2aeb83308ba7877ef6dcf94b8613eb40
Tasnime1 <73439861+Tasnime1@users.noreply.github.com>
2020-11-03 02:37:52 +0200

    Update arduino.cpp

--- Controller/arduino.cpp
+++ Controller/arduino.cpp
@@ -18,7 +18,7 @@ Arduino::Arduino(QWidget *parent)
     arduino_port_name = "";
     arduino = new QSerialPort;
 
-    joy = new Joystick();
+    joy = new Joystick;
 
     qDebug() << "Number of available ports: " << QSerialPortInfo::availablePorts().length();
     foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
@@ -35,34 +35,41 @@ Arduino::Arduino(QWidget *parent)
 
 
 
-    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
-        if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
-            if(serialPortInfo.vendorIdentifier() == arduino_uno_vendor_id){
-                if(serialPortInfo.productIdentifier() == arduino_uno_product_id){
-                    arduino_port_name = serialPortInfo.portName();
-                    arduino_is_available = true;
-                }
-            }
-        }
-    }
+//    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
+//        if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
+//            if(serialPortInfo.vendorIdentifier() == arduino_uno_vendor_id){
+//                if(serialPortInfo.productIdentifier() == arduino_uno_product_id){
+//                    arduino_port_name = serialPortInfo.portName();
+//                    qDebug()<<serialPortInfo.portName();
+//                    arduino_is_available = true;
+//                }
+//            }
+//        }
+//    }
 
-    if(arduino_is_available){
+//    if(arduino_is_available){
         // open and configure the serialport
-        arduino->setPortName(arduino_port_name);
+        arduino->setPortName("COM6");
         arduino->open(QSerialPort::WriteOnly);
         arduino->setBaudRate(QSerialPort::Baud9600);
         arduino->setDataBits(QSerialPort::Data8);
         arduino->setParity(QSerialPort::NoParity);
         arduino->setStopBits(QSerialPort::OneStop);
         arduino->setFlowControl(QSerialPort::NoFlowControl);
-    }else{
-        // give error message if not available
-        QMessageBox::warning(this, "Port Error", "Couldn't find an Arduino!");
 
-        connect(joy,SIGNAL(move(QString)),this,SLOT(moveSlot(QString)));
+//    }else{
+//        // give error message if not available
+//        QMessageBox::warning(this, "Port Error", "Couldn't find an Arduino!");
 
-    }
+
+        connect(joy,SIGNAL(move(QString)),this,SLOT(movementSlot(QString)));
+
+        myTimer= new QTimer();
+        connect(myTimer,SIGNAL(timeout()),this,SLOT(RecieveSlot()));
+        myTimer->start(100);
+
 }
+//}
 
 Arduino::~Arduino()
 {
@@ -70,31 +77,38 @@ Arduino::~Arduino()
 
 }
 
-void Arduino::SendRecieve(QString(command))
+void Arduino::RecieveSlot()
 {
-    ui->label_5->setText(command);
-    if(arduino->isWritable()){
-        arduino->write(command.toStdString().c_str());
-    }else{
-        qDebug() << "Couldn't write to serial!";
-    }
 
     if(arduino->isReadable())
     {
         Sensors=arduino->readAll();
         qDebug()<<Sensors; //to check data is correct
         ui->label_4->setText(Sensors);
-    } else
-    {
-        qDebug()<<"Can't read data from Serial Port(Arduino)!";
     }
+//    else
+//    {
+//        qDebug()<<"Can't read data from Serial Port(Arduino)!";
+//    }
 
 }
 
 
 
 
-void Arduino::moveSlot(QString(movement)){
+void Arduino::movementSlot(QString(movement)){
+
     Command= movement + "H"+ ui->lineEdit2->text() +"V"+ ui->lineEdit1->text();
-    qDebug()<<Command;
+
+    if(CommandP!=Command){
+        CommandP=Command;
+
+    qDebug()<<"NEW COMMAND->"+Command;
+    if(arduino->isWritable()){
+        arduino->write(Command.toStdString().c_str());
+        qDebug()<<"Sending to Arduino";
+    }else{
+        qDebug() << "Couldn't write to serial!";
+    }
+                          }
 }
